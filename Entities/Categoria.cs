495d5764033127b81d60095ab6d6ea58f337c587
Eynys Mcken <eynysm@gmail.com>
2019-04-28 19:01:44 -0500

    hkhhkkjgk

---
+++ Entities/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+	class Categoria
+	{
+		public string codigo_categoria { get; set; }
+		public string nombre_categoria { get; set; }
+	}
+}
